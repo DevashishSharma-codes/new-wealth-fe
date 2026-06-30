a1f6ad1803e949b4907e3b84f4ee3763cf5a1e7c
Devashish Sharma <devashish2157@gmail.com>
2026-06-30 14:02:54 +0530

    Frontend

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://wealth-wisdom-six.vercel.app',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
